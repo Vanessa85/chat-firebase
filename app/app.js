697595ee9838b3a3b87530e719618fa2089860ed
Vanessa Milagros <vanessapr.85@gmail.com>
2016-06-01 19:40:20 -0500

    initial proyect

---
+++ app/app.js
@@ -0,0 +1,18 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('appChat', ['ngRoute'])
+    .config(config);
+
+  config.$inject = ['$routeProvider'];
+  function config($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/views/home.html',
+        controller: 'HomeController'
+      });
+  }
+
+
+})();
