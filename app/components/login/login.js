b8c01472c827cfd2c865629397e81aa3582113f1
Vanessa Paco <vanessapr.85@gmail.com>
2017-08-19 09:04:54 -0500

    added .editorconfig

--- app/components/login/login.js
+++ app/components/login/login.js
@@ -1,38 +1,34 @@
 !function(angular) {
-    'use strict';
-    var loginComponent = {
-        templateUrl: 'app/components/login/login.html',
-        controller: LoginCtrl
-    };
+  'use strict';
+  var loginComponent = {
+    templateUrl: 'app/components/login/login.html',
+    controller: LoginCtrl
+  };
 
-    LoginCtrl.$inject = ['$state', 'firebaseApp'];
-    function LoginCtrl($state, firebaseApp) {
-        var vm = this;
-        vm.loginFB = loginFB;
-        vm.loginTW = loginTW;
+  LoginCtrl.$inject = ['$state', 'firebaseApp'];
+  function LoginCtrl($state, firebaseApp) {
+    var vm = this;
+    vm.loginFB = loginFB;
+    vm.loginTW = loginTW;
 
+    //////////
 
-        //////////
-
-        function loginFB() {
-          firebaseApp.authFB().then(function() {
-            $state.go('home');
-          }).catch(function(err) {
-            console.log('weeor', err);
-          });
-        }
-
-        function loginTW() {
-          $state.go('home');
-        }
-
+    function loginFB() {
+      firebaseApp.authFB().then(function() {
+        $state.go('home');
+      }).catch(function(err) {
+        console.log('weeor', err);
+      });
     }
 
+    function loginTW() {
+      $state.go('home');
+    }
 
+  }
 
-    angular
-        .module('appChat')
-        .component('login', loginComponent);
-
+  angular
+    .module('appChat')
+    .component('login', loginComponent);
 
 }(window.angular);
